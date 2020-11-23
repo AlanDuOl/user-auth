826216d1887f802bade90c808e9355037dfd46f6
alanduarte90 <alanduarte90@gmail.com>
2020-11-23 15:06:18 -0300

    add password constraints to register component

--- web/src/api-auth/register/register.component.ts
+++ web/src/api-auth/register/register.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { User } from '../../models';
-import { apiPath } from '../../constants';
 import { DialogService } from '../dialog.service';
 import { valueConstraints } from '../../utils';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { uiPath } from 'src/constants';
 
 @Component({
   selector: 'app-register',
@@ -38,10 +39,14 @@ export class RegisterComponent implements OnInit {
     ]),
   });
 
-  constructor(private http: HttpClient, private dialog: DialogService) { }
+  requestError = {
+    message: 'Validation failed',
+    on: false
+  }
+
+  constructor(private auth: AuthService, private dialog: DialogService, private router: Router) { }
 
   ngOnInit(): void {
-    console.log(this.form.get('password'));
   }
 
   canDeactivate(): Observable<boolean> {
@@ -58,11 +63,29 @@ export class RegisterComponent implements OnInit {
   }
 
   handleSubmit(): void {
-    console.log(this.form)
+    if (this.form.valid) {
+      const user = this.getRegisterData();
+      this.auth.register(user).subscribe(
+        res => {
+          console.log('user create', res);
+          this.router.navigate([uiPath.login]);
+        },
+        err => {
+          this.requestError.on = true;
+          this.requestError.message = err.error.message;
+        }
+      );
+    }
   }
 
-  submitData(data: User): Observable<any> {
-    return this.http.post<any>(apiPath.register, data);
+  private getRegisterData(): User {
+    const user: User = {
+      name: this.form.get('name').value,
+      email: this.form.get('email').value,
+      password: this.form.get('password').value,
+      confirmPassword: this.form.get('confirmPassword').value
+    }
+    return user;
   }
 
 }
