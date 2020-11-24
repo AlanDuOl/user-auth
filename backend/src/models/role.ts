c70fefeeb4ee15ae9fde727ff21297e21505d4ac
alanduarte90 <alanduarte90@gmail.com>
2020-11-24 11:28:24 -0300

    change table name to Role

--- backend/src/models/role.ts
+++ backend/src/models/role.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-@Entity('roles')
+@Entity()
 export default class Role {
     @PrimaryGeneratedColumn('increment')
     id: number;
