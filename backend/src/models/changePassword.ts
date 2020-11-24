fcdfc50412b2c4d2eb5b34972eba3ace0b169310
alanduarte90 <alanduarte90@gmail.com>
2020-11-24 18:54:05 -0300

    create changepassword model

---
+++ backend/src/models/changePassword.ts
@@ -0,0 +1,17 @@
+import {Column, Entity, JoinColumn, OneToOne, PrimaryColumn} from "typeorm";
+import User from "./user";
+
+@Entity()
+export class ChangePassword {
+
+    @PrimaryColumn()
+    token: string;
+
+    @Column()
+    expiresAt: Date;
+
+    @OneToOne(() => User, user => user.verification)
+    @JoinColumn()
+    user: User;
+    
+}
