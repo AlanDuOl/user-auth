d2800c10dbcc749287926e53eca790731dd3e607
alanduarte90 <alanduarte90@gmail.com>
2020-11-28 15:58:04 -0300

    finish(component) send-code component

--- web/src/api-auth/send-code/send-code.component.spec.ts
+++ web/src/api-auth/send-code/send-code.component.spec.ts
@@ -2,6 +2,7 @@ import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable, of, throwError } from 'rxjs';
+import { delay, tap } from 'rxjs/operators';
 import { uiPath } from 'src/constants';
 import { AuthService } from '../auth.service';
 
@@ -17,13 +18,13 @@ describe('SendCodeComponent', () => {
 
   beforeEach(async(() => {
     mockRouter = {
-      navigate: jasmine.createSpy('navigate').and.callFake(() => {})
+      navigate: jasmine.createSpy('navigate').and.callFake(() => { })
     },
-    mockAuth = {
-      requestResetCode(): Observable<any> { return of({ message: 'Code sent' }) }
-    }
+      mockAuth = {
+        sendResetCode(): Observable<any> { return of({ message: 'Code validated' }) }
+      }
     TestBed.configureTestingModule({
-      declarations: [ SendCodeComponent ],
+      declarations: [SendCodeComponent],
       imports: [
         ReactiveFormsModule
       ],
@@ -32,7 +33,7 @@ describe('SendCodeComponent', () => {
         { provide: AuthService, useValue: mockAuth },
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -47,13 +48,13 @@ describe('SendCodeComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('#requestCode should redirect to sendCode', fakeAsync(() => {
+  it('#sendCode should redirect to resetPassword', fakeAsync(() => {
     // define expected param
-    const expectedRoute = [`/${uiPath.sendCode}`];
+    const expectedRoute = [`/${uiPath.resetPassword}`];
 
-    // make form control valid and call #requestCode
-    component.form.get('email').setValue('alan@gmail.com');
-    component.requestCode();
+    // make form control valid and call #sendCode
+    component.form.get('code').setValue('aeid238sdsjas212');
+    component.sendCode();
     tick();
 
     // assert for navigate call
@@ -73,11 +74,11 @@ describe('SendCodeComponent', () => {
     // define return error
     const mockError = { error: { message: 'Error in request test' } };
     // make request return error
-    spyOn(auth, 'requestResetCode').and.returnValue(throwError(mockError));
+    spyOn(auth, 'sendResetCode').and.returnValue(throwError(mockError));
 
-    // make form control valid and call #requestCode
-    component.form.get('email').setValue('alan@gmail.com');
-    component.requestCode();
+    // make form control valid and call #sendCode
+    component.form.get('code').setValue('aeid238sdsjas212');
+    component.sendCode();
     tick();
 
     // assert message result to equal the error message in mockError
@@ -119,5 +120,40 @@ describe('SendCodeComponent', () => {
     el = document.querySelector('.error-text');
     // assert
     expect(el.textContent).toBe(message);
+  }));
+
+
+  it('#sendCode should not set isLoading and make the request if form data is invalid', () => {
+    // form is invalid as default
+    // test if #isLoading is false before call #sendCode
+    expect(component.isLoading).toBe(false);
+    // call #sendCode
+    component.sendCode();
+    // #isLoading value won't change
+    expect(component.isLoading).toBe(false);
+  });
+
+  it('#isLoading should be true while request does not complete', fakeAsync(() => {
+    // make form control valid to allow #isLoading reasignment
+    component.form.get('code').setValue('aeid238sdsjas212');
+
+    // #isLoading should be false before #sendCode is called
+    expect(component.isLoading).toBe(false);
+
+    // define async request
+    spyOn(auth, 'sendResetCode').and.returnValue(of({ message: 'test request' }).pipe(
+      tap(() => {
+        // assert for #isLoading to be true before the request end
+        expect(component.isLoading).toBe(true)
+      }),
+      // set request to wait 2 seconds to return
+      delay(2000)
+    ));
+    // run delayed request
+    component.sendCode();
+    // simulate delay of 2 seconds
+    tick(2000);
+    // #isLoading should be false after the request completes
+    expect(component.isLoading).toBe(false);
   }));
 });
--- web/src/api-auth/send-code/send-code.component.ts
+++ web/src/api-auth/send-code/send-code.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import { uiPath } from 'src/constants';
 
 @Component({
   selector: 'app-send-code',
@@ -11,7 +13,7 @@ import { BehaviorSubject } from 'rxjs';
 export class SendCodeComponent implements OnInit {
 
   form = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required])
+    code: new FormControl('', [Validators.required])
   })
   isLoading = false;
   message: BehaviorSubject<string | null> = new BehaviorSubject(null);
@@ -21,17 +23,17 @@ export class SendCodeComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  requestCode() {
+  sendCode() {
     // if form data is valid, request reset code
     if (this.form.valid) {
       this.isLoading = true;
-      const email = this.form.get('email').value;
-      this.auth.requestResetCode(email).subscribe(
+      const code = this.form.get('code').value;
+      this.auth.sendResetCode(code).subscribe(
         () => {
           this.isLoading = false;
-          this.router.navigate([`/${uiPath.sendCode}`]);
+          this.router.navigate([`/${uiPath.resetPassword}`]);
         },
-        err => {
+        (err: any) => {
           this.message.next(err.error.message);
           this.isLoading = false;
         }
