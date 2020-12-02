6950ed9c1c4f44170643ae5ced0d03ca36179558
alanduarte90 <alanduarte90@gmail.com>
2020-12-02 16:00:43 -0300

    add route handle admin user request

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import accountController from './controllers/accountController';
 import genericController from './controllers/genericController';
 import authenticationHandler from './middlewares/authHandler';
+import { authorizationHandler } from './middlewares/authorizationHandler';
 
 const routes = Router();
 
@@ -9,6 +10,8 @@ routes.get('/verify/:token', accountController.verifyAsync);
 routes.get('/sendemail/:id', accountController.sendVerificationAsync);
 routes.get('/validatecode/:token', accountController.validateResetCodeAsync);
 routes.get('/sendcode/:email', accountController.sendResetCodeAsync);
+routes.get('/admin/:id', authenticationHandler, authorizationHandler('Admin'),
+    genericController.getAdminAsync);
 routes.get('/public', genericController.getPublicAsync);
 routes.get('/user', authenticationHandler, genericController.getUserAsync);
 routes.post('/resetpassword', accountController.resetPassword);
