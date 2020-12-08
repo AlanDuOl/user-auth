971f51925eefd902e696df49c5569d3bbd1b297e
alanduarte90 <alanduarte90@gmail.com>
2020-12-08 20:36:34 -0300

    add tests to validation-error component

--- web/src/app/validation-error/validation-error.component.ts
+++ web/src/app/validation-error/validation-error.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 import { validationMessage } from '../../constants';
 
 @Component({
   selector: 'app-validation-error',
   templateUrl: './validation-error.component.html',
-  styleUrls: ['./validation-error.component.css']
+  styleUrls: ['./validation-error.component.scss']
 })
 export class ValidationErrorComponent implements OnInit {
 
-  @Input() errors: any;
+  @Input() errors: ValidationErrors;
   errorMessage: { [key: string]: string } = validationMessage;
 
   constructor() { }
