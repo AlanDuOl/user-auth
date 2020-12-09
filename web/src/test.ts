a3f4f7d0db2a7f8eafe052900953028283c0e3a7
alanduarte90 <alanduarte90@gmail.com>
2020-12-08 21:48:26 -0300

    finish front-end tests

--- web/src/test.ts
+++ web/src/test.ts
@@ -20,7 +20,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-// const context = require.context('./', true, /\.spec\.ts$/);
-const context = require.context('./', true, /not-found.component\.spec\.ts$/);
+const context = require.context('./', true, /\.spec\.ts$/);
+// const context = require.context('./', true, /reset-password.component\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
