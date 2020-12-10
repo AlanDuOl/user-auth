ae1b1248aea2f92654ef93e7d513b238626cb34a
alanduarte90 <alanduarte90@gmail.com>
2020-12-10 14:29:08 -0300

    set up basic env to run tests with mocha/chain/typescript

---
+++ backend/test/test.spec.ts
@@ -0,0 +1,13 @@
+
+import { expect } from 'chai';
+
+describe('Fake test', function() {
+    it('fake test', function() {
+        expect(true).to.equal(true);
+    });
+
+    
+    it('new fake test', function() {
+        expect(true).to.equal(true);
+    });
+});
